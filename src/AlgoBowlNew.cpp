aa7575247cc9e3d20fa633e1822bdffe3ef5a71f
Stephen Agee <sagee@mines.edu>
2018-02-17 19:07:08 -0700

    Added some new inputs, and created AlgoBowlNew.cppwhich uses a class to store the adjmatrix and do calculations for easy algorithm implementation

--- src/AlgoBowlNew.cpp
+++ src/AlgoBowlNew.cpp
@@ -16,48 +16,63 @@
 #include <set>
 using namespace std;
 
+class Solver{
+	public:
+		Solver(string inputFilename){
+			this->inputFilename = inputFilename;
+		}
+		void ImportData();
+		void DoAlgorithm();
+	private:
+		string inputFilename;
+		int num_vertices;
+		int num_edges;
+		vector <vector <int> > adjacency_matrix;
+
+};
+
 int main (){
 
+	Solver input1("InputFiles/stephenTest.txt");
+	input1.ImportData();
+	input1.DoAlgorithm();
+
+	return 0;
+}
+
+void Solver::ImportData(){
 	//first we need to read in the file
-	string input_filename = "InputFiles/mehtaHeuristicInput.txt";
-	//cout << "Enter the input filename: ";
-	//cin >> input_filename;
 	ifstream inputFile;
-	inputFile.open(input_filename);
+	inputFile.open(inputFilename);
 	if (!inputFile.is_open()){
 		cerr << "Couldn't open the file" << endl;
-		return 1;
+		return;
 	}
-
-	int num_vertices;
-	int num_edges;
+	//the first line of the file contains number of vertices and edges
 	inputFile >> num_vertices >> num_edges;
 	//make the adjacency matrix to store the points and weights
 	//it should be nxn where n is the number of vertices 
 	//This allows us to map to all other vertices
-	vector <vector <int> > adjacency_matrix(num_vertices, vector<int>(num_vertices));
-	
+	//will need to be set as class vector
+	vector <vector <int> > temp_matrix(num_vertices, vector<int>(num_vertices));
+
 	while (!inputFile.eof()){
 		int currentVert, connectingNode, weight;
 		inputFile >> currentVert >> connectingNode >> weight;
 		//we need to use -1 because the graph is 0 indexed
 		//we also need to add the values to eachother since both are adjacent
-		adjacency_matrix.at(currentVert - 1).at(connectingNode - 1) = weight;
-		adjacency_matrix.at(connectingNode - 1).at(currentVert - 1) = weight;
+		temp_matrix.at(currentVert - 1).at(connectingNode - 1) = weight;
+		temp_matrix.at(connectingNode - 1).at(currentVert - 1) = weight;
 	}
+	adjacency_matrix = temp_matrix;
+	inputFile.close();
+}
 
-	for (int i = 0; i < adjacency_matrix.size(); i++){
-		cout << "Row " << i << ": ";
-		for (int j = 0; j < adjacency_matrix.size(); j++){
-			cout << adjacency_matrix[i][j] << " ";
-		}
-		cout << endl;
-	}
 
+void Solver::DoAlgorithm(){
 	//define two vectors for the initial guess
 	vector <int> sideA (num_vertices/2);
 	vector <int> sideB (num_vertices/2);
-	
 	//initially, we will guess that we should just split the nodes in half
 	//from here forward we will denote node "1" as node "0" to make things simpler
 	for (int i = 0; i < num_vertices/2; i++){
@@ -67,50 +82,18 @@ int main (){
 		sideB.at(i-(num_vertices/2))=i;
 	}
 
-	cout << "sideA: ";
-	for (int i = 0; i < sideA.size(); i++){
-		cout << sideA[i] << " ";
-	}
-	cout << endl << "sideB: ";
-	for (int i = 0; i < sideA.size(); i++){
-		cout << sideB[i] << " ";
-	}
-	cout << endl;
 	//now that we have the split vectors, we find the cost
 	//TODO: make this a cost FUNCTION
-
 	int tot_cost = 0;
+
 	for (int a: sideA){
 		for (int b: sideB){
-			if (adjacency_matrix[a][b] != 0){
-			cout << tot_cost << " ";
-			cout << " Adding [" << a << " " << b << "] ";
 			tot_cost += adjacency_matrix[a][b];
-			cout << tot_cost << endl;
-			}	
 		}
 	}
+
 	cout << "total cost is: " << tot_cost << endl;
-
-	int max_cost = 0;
-	for (int a: sideA){
-		for (int b: sideB){
-			max_cost+=50;	
-		}
-	}
-
-	cout << "Max cost is: " << max_cost << endl;
-
-
-
-
-	inputFile.close();
-
-	//next we can use the algorithm to find the best split
-	//todo: finish this
-	//for indices in set, sum across in the matrix
-
-	return 0;
 }
+
 
 
--- src/inputCreate.cpp
+++ src/inputCreate.cpp
@@ -1,35 +1,56 @@
 #include <iostream>
 #include <fstream>
 #include <algorithm> 
+#include <vector>
+#include <string>
 using namespace std;
 
+const string OUTPUT_FILE = "stephenTest.txt";
+
 int main() {
-    int count = 0;
+    //create and shuffle nodes from 1 to 600
     vector <int> nodes;
     for (int i = 0; i<= 599; i++){
         nodes.push_back(i+1);
     }
-  random_shuffle(nodes.begin(), nodes.end());
-  ofstream myfile;
-  myfile.open ("mehtaHeuristicInput.txt");
-  for (int i = 0; i <= 299; i++){
-      for (int j = i+1; j <= 299; j++){
-            myfile << nodes.at(i) << " " <<  nodes.at(j) << " " << rand()%((50 - 40) + 1) + 40 << "\n";
-      }
-  }
-  myfile << nodes.at(299) << " " << nodes.at(300) << " " << 40 << "\n";
-  cout << nodes.at(299) << " " << nodes.at(300) << " " << 40 << endl;
-    for (int i = 300; i <= 599; i++){
-      for (int j = i+1; j <= 599; j++){
-            myfile << nodes.at(i) << " " <<  nodes.at(j) << " " << rand()%((50 - 40) + 1) + 40 << "\n";
-      }
-  }
-  myfile.close();
-    
-    
-for (int i = 0; i < nodes.size(); i++){
-    cout << nodes.at(i) << endl;
-}
+    //random_shuffle(nodes.begin(), nodes.end());
+
+    //create and open output file
+    ofstream outFile;
+    outFile.open (OUTPUT_FILE);
+    int edge_count = 0;
+    //for the first 300 nodes
+    for (int i = 0; i <= 299; i++){
+        //for nodes greater than current node
+        for (int j = i+1; j <= 299; j++){
+              //connect current node to greater nodes
+              outFile << nodes.at(i) << " " <<  nodes.at(j) 
+                      << " " << rand()%((50 - 40) + 1) + 40 << "\n";
+              //we connected an edge, increment edge counter
+              edge_count++;
+        }
+    }
+    //connect node from first group to second group
+    outFile << nodes.at(299) << " " << nodes.at(300) << " " << 40 << "\n";
+    //we connected an edge, increment edge counter
+    edge_count++;
+    cout << nodes.at(299) << " " << nodes.at(300) << " " << 40 << endl;
+    //for last 300 nodes
+      for (int i = 300; i <= 599; i++){
+        //for nodes greater than current node
+        for (int j = i+1; j <= 599; j++){
+              //connect current node to greater nodes
+              outFile << nodes.at(i) << " " <<  nodes.at(j) << " " 
+                      << rand()%((50 - 40) + 1) + 40 << "\n";
+              //we connected an edge, increment edge counter
+              edge_count++;
+        }
+    }
+    //close the output file
+    outFile.close();
+
+    cout << "Text file name: " << OUTPUT_FILE << " " 
+          << nodes.size() << " " << edge_count << endl;
     
     return 0;
 }

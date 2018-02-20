5de64b6a38e0a602167fbec767e3e09be0329279
Stephen Agee <sagee@mines.edu>
2018-02-19 20:37:52 -0700

    Updated AlgoBowlNew with better syntax

--- src/AlgoBowlNew.cpp
+++ src/AlgoBowlNew.cpp
@@ -18,13 +18,9 @@ using namespace std;
 
 class Solver{
 	public:
-		Solver(string inputFilename){
-			this->inputFilename = inputFilename;
-		}
-		void ImportData();
-		void DoAlgorithm();
+		bool import_data(string input_filename);
+		void run_simulated_annealing();
 	private:
-		string inputFilename;
 		int num_vertices;
 		int num_edges;
 		vector <vector <int> > adjacency_matrix;
@@ -33,67 +29,68 @@ class Solver{
 
 int main (){
 
-	Solver input1("InputFiles/stephenTest.txt");
-	input1.ImportData();
-	input1.DoAlgorithm();
+	Solver solver;
+	if (!solver.import_data("InputFiles/stephenTest.txt")){
+		cerr << "Failed to import data" << endl;
+		return 1;
+	}
+
+	solver.run_simulated_annealing();
 
 	return 0;
 }
 
-void Solver::ImportData(){
+bool Solver::import_data(string input_filename){
 	//first we need to read in the file
-	ifstream inputFile;
-	inputFile.open(inputFilename);
-	if (!inputFile.is_open()){
-		cerr << "Couldn't open the file" << endl;
-		return;
+	ifstream input_file;
+	input_file.open(input_filename);
+	if (!input_file.is_open()){
+		cerr << "Couldn't open the input file" << endl;
+		return false;
 	}
 	//the first line of the file contains number of vertices and edges
-	inputFile >> num_vertices >> num_edges;
-	//make the adjacency matrix to store the points and weights
-	//it should be nxn where n is the number of vertices 
-	//This allows us to map to all other vertices
-	//will need to be set as class vector
-	vector <vector <int> > temp_matrix(num_vertices, vector<int>(num_vertices));
+	input_file >> num_vertices >> num_edges;
 
-	while (!inputFile.eof()){
-		int currentVert, connectingNode, weight;
-		inputFile >> currentVert >> connectingNode >> weight;
-		//we need to use -1 because the graph is 0 indexed
+	//next we make the adjacency matrix to store the points and weights
+	//it should be nxn where n is the number of vertices 
+	vector <vector <int> > temp_adj_matrix(num_vertices, vector<int>(num_vertices));
+
+	while (!input_file.eof()){
+		int current_vert, connecting_node, weight;
+		input_file >> current_vert >> connecting_node >> weight;
+		//we need to subtract 1 since the graph is 1 indexed
 		//we also need to add the values to eachother since both are adjacent
-		temp_matrix.at(currentVert - 1).at(connectingNode - 1) = weight;
-		temp_matrix.at(connectingNode - 1).at(currentVert - 1) = weight;
+		temp_adj_matrix.at(current_vert - 1).at(connecting_node - 1) = weight;
+		temp_adj_matrix.at(connecting_node - 1).at(current_vert - 1) = weight;
 	}
-	adjacency_matrix = temp_matrix;
-	inputFile.close();
+	adjacency_matrix = temp_adj_matrix;
+	input_file.close();
+	return true;
 }
 
 
-void Solver::DoAlgorithm(){
+void Solver::run_simulated_annealing(){
 	//define two vectors for the initial guess
-	vector <int> sideA (num_vertices/2);
-	vector <int> sideB (num_vertices/2);
+	vector <int> side_a (num_vertices/2);
+	vector <int> side_b (num_vertices/2);
 	//initially, we will guess that we should just split the nodes in half
-	//from here forward we will denote node "1" as node "0" to make things simpler
 	for (int i = 0; i < num_vertices/2; i++){
-		sideA.at(i)=i;
+		side_a.at(i)=i;
 	}
 	for (int i = num_vertices/2; i < num_vertices; i++){
-		sideB.at(i-(num_vertices/2))=i;
+		side_b.at(i-(num_vertices/2))=i;
 	}
 
 	//now that we have the split vectors, we find the cost
 	//TODO: make this a cost FUNCTION
 	int tot_cost = 0;
 
-	for (int a: sideA){
-		for (int b: sideB){
+	for (int a: side_a){
+		for (int b: side_b){
 			tot_cost += adjacency_matrix[a][b];
 		}
 	}
 
 	cout << "total cost is: " << tot_cost << endl;
 }
-
-
 

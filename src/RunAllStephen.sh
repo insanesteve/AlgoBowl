1d577ac5f5586c5770a20dee0eba95cd5a91b2d0
Stephen Agee <sagee@mines.edu>
2018-02-21 14:56:04 -0700

    Added new input files

--- src/RunAllStephen.sh
+++ src/RunAllStephen.sh
@@ -2,9 +2,9 @@
 # We plan to run this script multiple times to ensure a good solution is found
 
 #!/bin/bash
-for group in {1..28}
+for group in {1..27}
 do
 	echo "Starting input_group$group.txt"
-	./AlgoBowl InputFiles/input_group$group.txt StephenOutputs/output_group$group.txt
+	./AlgoBowl InputFiles/input_group$group.txt StephenOutputs/output_group$group.txt &
 	echo "done."
 done

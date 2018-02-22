e39d491aa5b2ff4f1aa890a7a62008f6a53dd48e
Stephen Agee <sagee@isengard.Mines.EDU>
2018-02-22 07:33:07 -0700

    Overnight run from isengard

--- src/RunAllStephenMac.sh
+++ src/RunAllStephenMac.sh
@@ -2,7 +2,7 @@
 # We plan to run this script multiple times to ensure a good solution is found
 
 #!/bin/bash
-for int in {1..500}
+for int in {1..800}
 do
 	for group in {1..27}
 	do
@@ -11,4 +11,7 @@ do
 		echo "done."
 	done
 	wait
+	echo "Iteration:$int"
+	sleep 5
+
 done

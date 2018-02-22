3253186b38f663b6ad7ecd00d12ed2aef40ad377
Stephen Agee <sagee@isengard.Mines.EDU>
2018-02-22 00:29:28 -0700

    Added outputs from isengard (even though it says mac lol)

--- src/RunAllStephenMac.sh
+++ src/RunAllStephenMac.sh
@@ -2,9 +2,13 @@
 # We plan to run this script multiple times to ensure a good solution is found
 
 #!/bin/bash
-for group in {1..27}
+for int in {1..500}
 do
-	echo "Starting input_group$group.txt"
-	./AlgoBowl InputFiles/input_group$group.txt StephenMacOutputs/output_group$group.txt &
-	echo "done."
+	for group in {1..27}
+	do
+		echo "Starting input_group$group.txt"
+		./AlgoBowl InputFiles/input_group$group.txt StephenMacOutputs/output_group$group.txt &
+		echo "done."
+	done
+	wait
 done

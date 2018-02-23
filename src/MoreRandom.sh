4f3714854557a01b700c57b476f8d9e21dacf17b
Stephen Agee <sagee@mines.edu>
2018-02-23 16:44:17 -0700

    Added a change to be more random

---
+++ src/MoreRandom.sh
@@ -0,0 +1,17 @@
+# This script runs AlgoBowl on all AlgoBowl inputs. 
+# We plan to run this script multiple times to ensure a good solution is found
+
+#!/bin/bash
+for int in {1..500}
+do
+	for group in {1..27}
+	do
+		echo "Starting input_group$group.txt"
+		./AlgoBowlNew PlzHelp/input_group$group.txt AlgoBowlMoreRandom/output_group$group.txt &
+		echo "done."
+	done
+	wait
+	echo "Iteration:$int"
+	sleep 5
+
+done

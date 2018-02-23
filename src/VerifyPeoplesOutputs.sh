3b05198661505d76651271572c2da32eb0528c7f
Alexander Santilli <asantilli@isengard.Mines.EDU>
2018-02-23 13:19:47 -0700

    Fixed once again...

--- src/VerifyPeoplesOutputs.sh
+++ src/VerifyPeoplesOutputs.sh
@@ -1,4 +1,5 @@
 for group in {1..28}
 do
-./CheckOutput InputFiles/input_group13.txt OutputFilesToVerify/output_from_$group._to_13.txt
+echo "Group $group 's output:\r\n"
+./CheckOutput InputFiles/input_group13.txt OutputFilesToVerify/output_from_$group.txt
 done

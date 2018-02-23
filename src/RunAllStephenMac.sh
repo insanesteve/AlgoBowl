225fd82e09d5cc87f6de38df0ca6aed822cc309d
Stephen Agee <sagee@isengard.Mines.EDU>
2018-02-23 00:34:55 -0700

    Script modifications for stephen

--- src/RunAllStephenMac.sh
+++ src/RunAllStephenMac.sh
@@ -7,7 +7,7 @@ do
 	for group in {1..27}
 	do
 		echo "Starting input_group$group.txt"
-		./AlgoBowl InputFilesNew/input_group$group.txt StephenMacOutputs/output_group$group.txt &
+		./AlgoBowlLong InputFilesNew/input_group$group.txt StephenMacOutputs/output_group$group.txt &
 		echo "done."
 	done
 	wait
